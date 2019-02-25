9c878d4ec49028a22008cd211c62f0ddda3236a9
Francis Ilechukwu <francis94c@gmail.com>
2019-02-25 19:19:35 +0100

    Started Splint Update.

---
+++ SplintUpdate/src/com/cynobit/splint_update/Main.java
@@ -0,0 +1,10 @@
+package com.cynobit.splint_update;
+
+/**
+ * Created by Francis Ilechukwu 2/25/2019.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Checking for updates...");
+    }
+}
